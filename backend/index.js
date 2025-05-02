92278215bb22978f8aec7f6e3b1ea77597373c77
sangeetasonal123@gmail.com <sangeetasonal123@gmail.com>
2025-05-02 17:19:39 +0530

    Fix scrollbar flicker and handle empty filteredData

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ import { router as usersRouter } from './routes/users.js';
 
 const app = express();
 app.use(cors());
+app.get("/", (req, res) => {
+  res.send(" Server is working!!!!!!");
+});
+
 app.use("/api/users", usersRouter);
 
 const PORT = 5000;
